928a65cfc7984da4efc4ce1dde60110b904456e4
tasszz2k <tasszz2k@gmail.com>
2022-01-14 09:37:21 +0700

    init project

---
+++ src/main/java/com/labate/mentoringme/MentoringMeApplication.java
@@ -0,0 +1,13 @@
+package com.labate.mentoringme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoringMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentoringMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/labate/mentoringme/MentoringMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labate.mentoringme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoringMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

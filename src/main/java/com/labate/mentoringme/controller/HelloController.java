4329f661d9f497cfc270ff3bf83bda56a852bd19
tasszz2k <tasszz2k@gmail.com>
2022-02-10 01:02:20 +0700

    test GitHub action

--- src/main/java/com/labate/mentoringme/controller/HelloController.java
+++ src/main/java/com/labate/mentoringme/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("")
     public String sayHello() {
-        return "[LABATE] Mentoring Me !!!";
+        return "[LABATE] Mentoring Me +++";
     }
 
 }

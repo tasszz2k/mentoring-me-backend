f7514601f9d7adc5b209b321bc393d959502e8ec
tasszz2k <tasszz2k@gmail.com>
2022-02-22 19:15:00 +0700

    feat(authentication): check valid password

---
+++ src/main/java/com/labate/mentoringme/validator/PasswordValueMatch.java
@@ -0,0 +1,34 @@
+package com.labate.mentoringme.validator;
+
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+
+@Target({ TYPE, ANNOTATION_TYPE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = PasswordFieldsValueMatchValidator.class)
+@Documented
+public @interface PasswordValueMatch {
+
+
+    String message() default "Fields values don't match!";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+
+    String field();
+
+    String fieldMatch();
+
+    @Target({ ElementType.TYPE })
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface List {
+        PasswordValueMatch[] value();
+    }
+}

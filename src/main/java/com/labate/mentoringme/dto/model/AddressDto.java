f98e07be19d79866fc2a1b7ef10731c32c35e0ac
tasszz2k <tasszz2k@gmail.com>
2022-02-26 12:01:38 +0700

    update(address): response ignore null value

--- src/main/java/com/labate/mentoringme/dto/model/AddressDto.java
+++ src/main/java/com/labate/mentoringme/dto/model/AddressDto.java
@@ -1,10 +1,12 @@
 package com.labate.mentoringme.dto.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Value;
 
 @Value
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class AddressDto {
   Long id;
   Integer type;

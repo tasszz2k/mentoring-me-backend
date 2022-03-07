e022bedf55950e2c1e8ff4a5613677ac74cffbd7
tasszz2k <tasszz2k@gmail.com>
2022-03-07 23:09:26 +0700

    feat(internationalization): add LanguageService

---
+++ src/main/java/com/labate/mentoringme/internationalization/LocaleConfig.java
@@ -0,0 +1,57 @@
+package com.labate.mentoringme.internationalization;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+@Configuration
+public class LocaleConfig extends AcceptHeaderLocaleResolver implements WebMvcConfigurer {
+
+    public LocaleConfig() {
+        super();
+        setDefaultLocale(LocaleConstants.DEFAULT_LOCALE);
+        setSupportedLocales(new ArrayList<>(LocaleConstants.SUPPORT_LOCALES.values()));
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageResource = new ReloadableResourceBundleMessageSource();
+        messageResource.setBasenames("classpath:i18n/messages");
+        messageResource.setDefaultEncoding("UTF-8");
+        messageResource.setCacheSeconds(60);
+        Locale.setDefault(LocaleConstants.DEFAULT_LOCALE);
+        return messageResource;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+        localeInterceptor.setParamName("lang");
+        registry.addInterceptor(localeInterceptor);
+    }
+
+    @Override
+    public Locale resolveLocale(HttpServletRequest request) {
+        String language = request.getHeader(HttpHeaders.ACCEPT_LANGUAGE);
+        if (!StringUtils.hasText(language)) {
+            return getDefaultLocale();
+        }
+        List<Locale.LanguageRange> languageRanges = Locale.LanguageRange.parse(language);
+        Locale foundLocale = Locale.lookup(languageRanges, getSupportedLocales());
+        if (foundLocale == null)
+            return getDefaultLocale();
+        return foundLocale;
+    }
+}
---
+++ src/main/java/com/labate/mentoringme/internationalization/impl/HttpRequestLanguageService.java
@@ -0,0 +1,52 @@
+package com.labate.mentoringme.internationalization.impl;
+
+import com.labate.mentoringme.internationalization.LanguageService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.MessageFormat;
+import java.util.Locale;
+
+@Component
+@Slf4j
+public class HttpRequestLanguageService implements LanguageService {
+    private final MessageSource messageSource;
+
+    @Autowired
+    public HttpRequestLanguageService(
+            MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @Override
+    public Locale getCurrentLocale() {
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        if (!(requestAttributes instanceof ServletRequestAttributes)) {
+            return Locale.getDefault();
+        }
+        HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+        return request.getLocale();
+    }
+
+    @Override
+    public String getMessage(String messageCode, String defaultMessage, Object... params) {
+        Locale currentLocale = getCurrentLocale();
+        try {
+            return messageSource.getMessage(messageCode, params, currentLocale);
+        } catch (Exception e) {
+            if (e instanceof NoSuchMessageException) {
+                log.warn("Could not find message {} for locale {}. Using default message", messageCode, currentLocale);
+            } else {
+                log.warn("Failed to get message {} for locale {}. Using default message", messageCode, currentLocale, e);
+            }
+            return MessageFormat.format(defaultMessage, params);
+        }
+    }
+}

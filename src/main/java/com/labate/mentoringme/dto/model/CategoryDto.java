a475ca28656c1d239538670df7c5ab9df6d60e21
minhthao0606 <minhthao0606@gmail.com>
2022-03-09 23:32:07 +0700

    (feat) quiz-management: resolve conflict

--- src/main/java/com/labate/mentoringme/dto/model/CategoryDto.java
+++ src/main/java/com/labate/mentoringme/dto/model/CategoryDto.java
@@ -1,11 +1,8 @@
 package com.labate.mentoringme.dto.model;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import lombok.Data;
 
 @Data
-@JsonInclude(Include.NON_NULL)
 public class CategoryDto {
 
   private Long id;

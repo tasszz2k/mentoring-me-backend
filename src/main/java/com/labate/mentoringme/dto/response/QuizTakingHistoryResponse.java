0ac6f99924e205780d2f2a394c7750f03c74d4a7
minhthao0606 <minhthao0606@gmail.com>
2022-03-14 18:44:42 +0700

    (feat) quiz-management: fix bug check quiz result

--- src/main/java/com/labate/mentoringme/dto/response/QuizTakingHistoryResponse.java
+++ src/main/java/com/labate/mentoringme/dto/response/QuizTakingHistoryResponse.java
@@ -1,12 +1,15 @@
 package com.labate.mentoringme.dto.response;
 
-import lombok.Data;
-
 import java.util.Date;
+import org.springframework.format.annotation.DateTimeFormat;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
 
 @Data
 public class QuizTakingHistoryResponse {
   private String title;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
+  @DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
   private Date created;
   private Integer score;
   private Integer numberOfTrue;

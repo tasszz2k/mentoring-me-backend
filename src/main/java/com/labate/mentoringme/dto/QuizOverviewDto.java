87ef7ce299eac3b412293f9295207c79dcc74405
minhthao0606 <minhthao0606@gmail.com>
2022-03-25 12:15:02 +0700

    (feat) feedback: update get feedback rating

--- src/main/java/com/labate/mentoringme/dto/QuizOverviewDto.java
+++ src/main/java/com/labate/mentoringme/dto/QuizOverviewDto.java
@@ -1,12 +1,9 @@
 package com.labate.mentoringme.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import lombok.Data;
 
 @Data
-@JsonInclude(Include.NON_NULL)
 public class QuizOverviewDto {
   private Long id;
   private String title;

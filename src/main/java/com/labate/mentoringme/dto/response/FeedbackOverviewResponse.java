66b69f49ead2991301f453c49062ab1ef2d381dd
minhthao0606 <minhthao0606@gmail.com>
2022-03-27 15:15:58 +0700

    (feat) feedbacks: update get feedback overview

--- src/main/java/com/labate/mentoringme/dto/response/FeedbackOverviewResponse.java
+++ src/main/java/com/labate/mentoringme/dto/response/FeedbackOverviewResponse.java
@@ -1,6 +1,7 @@
 package com.labate.mentoringme.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -9,8 +10,15 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @RequiredArgsConstructor
+@Builder
 public class FeedbackOverviewResponse {
 
   private FeedbackResponse myFeedback;
-  private final Double overallRating;
+  private Double overallRating;
+  private Integer numberOfFeedback;
+  private Integer numberOfOneRating;
+  private Integer numberOfTwoRating;
+  private Integer numberOfThreeRating;
+  private Integer numberOfFourRating;
+  private Integer numberOfFiveRating;
 }

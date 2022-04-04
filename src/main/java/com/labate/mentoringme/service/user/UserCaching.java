2581f353f1fcd851b37580e60809f464af95bfe4
tasszz2k <tasszz2k@gmail.com>
2022-04-04 10:42:13 +0700

    update(user): remove unused

--- src/main/java/com/labate/mentoringme/service/user/UserCaching.java
+++ src/main/java/com/labate/mentoringme/service/user/UserCaching.java
@@ -14,22 +14,6 @@ public class UserCaching {
 
   private final UserRepository userRepository;
 
-  // @Value("${labate.cache.user.expiration-time}")
-  // private int timeCacheUser;
-  //
-  // public final LoadingCache<Long, BasicUserInfo> basicUserInfoCache =
-  //     CacheBuilder.newBuilder()
-  //         .maximumSize(AppConstant.MAXIMUM_CACHE_SIZE)
-  //         .expireAfterWrite(6, TimeUnit.HOURS)
-  //         .build(
-  //             new CacheLoader<>() {
-  //               @Override
-  //               public BasicUserInfo load(@NotNull final Long userId) {
-  //                 var prj = userRepository.findBasicUserInfoById(userId);
-  //                 return UserMapper.toBasicUserInfo(prj);
-  //               }
-  //             });
-
   /**
    * Only external method calls coming in through the proxy are intercepted. This means that
    * self-invocation, in effect, a method within the target object calling another method of the

fbe20af62717dea59b2bc2aeb6588017407e0888
tasszz2k <tasszz2k@gmail.com>
2022-04-07 23:01:59 +0700

    update(notification): update format date notification

--- src/main/java/com/labate/mentoringme/dto/model/NotificationDto.java
+++ src/main/java/com/labate/mentoringme/dto/model/NotificationDto.java
@@ -1,6 +1,8 @@
 package com.labate.mentoringme.dto.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -10,6 +12,18 @@ public class NotificationDto {
   private String title;
   private String message;
   private boolean isRead;
+
+  @JsonFormat(
+      shape = JsonFormat.Shape.STRING,
+      pattern = "yyyy-MM-dd HH:mm:ss",
+      timezone = "Asia/Bangkok")
+  @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
   private Date createdDate;
+
+  @JsonFormat(
+      shape = JsonFormat.Shape.STRING,
+      pattern = "yyyy-MM-dd HH:mm:ss",
+      timezone = "Asia/Bangkok")
+  @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
   private Date modifiedDate;
 }

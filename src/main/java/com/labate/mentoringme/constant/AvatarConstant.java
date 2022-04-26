2af218c809482f32d2b5b2e518e9f2fddb14d7a4
tasszz2k <tasszz2k@gmail.com>
2022-04-26 21:54:55 +0700

    feat(user profile):CAPSTONE-458: default avt

--- src/main/java/com/labate/mentoringme/constant/AvatarConstant.java
+++ src/main/java/com/labate/mentoringme/constant/AvatarConstant.java
@@ -1,14 +1,14 @@
 package com.labate.mentoringme.constant;
 
 public enum AvatarConstant {
-  ADMIN("https://storage.googleapis.com/labate-image/images/admin.jpeg"),
-  MODERATOR("https://storage.googleapis.com/labate-image/images/moderator.jpeg"),
-  MENTOR_MALE("https://storage.googleapis.com/labate-image/images/mentor-male.jpeg"),
-  MENTOR_FEMALE("https://storage.googleapis.com/labate-image/images/mentor-female.jpeg"),
-  MENTOR_OTHER("https://storage.googleapis.com/labate-image/images/mentor-other.jpeg"),
-  USER_MALE("https://storage.googleapis.com/labate-image/images/user-male.jpeg"),
-  USER_FEMALE("https://storage.googleapis.com/labate-image/images/user-female.jpeg"),
-  USER_OTHER("https://storage.googleapis.com/labate-image/images/user-other.jpeg"),
+  ADMIN("https://storage.googleapis.com/labate-image/images/admin.jpg"),
+  MODERATOR("https://storage.googleapis.com/labate-image/images/moderator.jpg"),
+  MENTOR_MALE("https://storage.googleapis.com/labate-image/images/mentor-male.jpg"),
+  MENTOR_FEMALE("https://storage.googleapis.com/labate-image/images/mentor-female.jpg"),
+  MENTOR_OTHER("https://storage.googleapis.com/labate-image/images/mentor-other.jpg"),
+  USER_MALE("https://storage.googleapis.com/labate-image/images/user-male.jpg"),
+  USER_FEMALE("https://storage.googleapis.com/labate-image/images/user-female.jpg"),
+  USER_OTHER("https://storage.googleapis.com/labate-image/images/user-other.jpg"),
   ;
 
   private final String url;
